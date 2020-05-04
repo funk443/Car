2ddc85930573e43730fa8b2e645478a9d643d1e0
funk443 <funk443@yandex.com>
2020-05-04 23:28:09 +0800

    include, setup
    Included <SoftwareSerial.h> and <Servo.h>.
    Serial setup and bluetooth setup done

--- Car.ino
+++ Car.ino
@@ -1,6 +1,15 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+#define rx A1
+#define tx A0
+
+SoftwareSerial BTtrans(rx, tx);
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  BTtrans.begin(9600);
 }
 
 void loop() {
